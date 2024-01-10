78538b7d185f03c413b733b7ce23597f5a02e765
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-01-10 19:09:03 +0200

    pass 2st stage decode integer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serde_json;
+use serde_json::{self, json};
 use std::env;
 
 // Available if you need it!
@@ -7,13 +7,19 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    let bencode_identifier = encoded_value.chars().next().unwrap();
+    if bencode_identifier.is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
+        return json!(string);
+    } else if bencode_identifier == 'i' {
+        let e_index = encoded_value.find('e').unwrap();
+        let number_string = &encoded_value[1..e_index];
+        let number = number_string.parse::<i64>().unwrap();
+        return json!(number);
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
