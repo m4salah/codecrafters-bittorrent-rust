eddb5da766da237edecf83b3ec84ae11d26c9e06
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-01-11 21:37:29 +0200

    calculate info hash for the torrent file

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use std::{
     fs,
 };
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_bytes::ByteBuf;
+use sha1::{Digest, Sha1};
 
 use crate::bendecoder::decode_bencoded_value;
 mod bendecoder;
@@ -12,7 +14,7 @@ mod bendecoder;
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 // Usage: your_bittorrent.sh info "<file>.torrent"
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct TorrentInfo {
     pub announce: String,
     pub info: Info,
@@ -26,23 +28,38 @@ impl Display for TorrentInfo {
     }
 }
 
-#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+#[derive(Deserialize, Serialize, Debug)]
 struct Info {
     pub length: usize,
+    pub name: String,
+    #[serde(rename = "piece length")]
+    pub piece_length: usize,
+    pub pieces: ByteBuf,
 }
 
 impl Display for Info {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(format!("Length: {}", self.length).as_str())
+        f.write_str(format!("Length: {}", self.length).as_str())?;
+        f.write_char('\n')?;
+        f.write_str(format!("Name: {}", self.name).as_str())?;
+        f.write_char('\n')?;
+        f.write_str(format!("Piece Length: {}", self.piece_length).as_str())
     }
 }
 
-fn parse_torrent(torrent_path: &str) -> Result<TorrentInfo, anyhow::Error> {
+fn parse_torrent(torrent_path: &str) -> Result<(TorrentInfo, String), anyhow::Error> {
     let torrent_byte = fs::read(torrent_path)?;
     let decoded: TorrentInfo = serde_bencode::from_bytes(&torrent_byte)?;
+    let bytes = serde_bencode::to_bytes(&decoded.info).unwrap();
+    let mut hasher = Sha1::new();
 
-    Ok(decoded)
+    hasher.update(bytes);
+    let hash = hasher.finalize();
+    let hexed = hex::encode(hash);
+    Ok((decoded, hexed))
 }
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
@@ -57,7 +74,8 @@ fn main() {
     } else if command == "info" {
         let torrent_path = &args[2];
         let decoded_value = parse_torrent(&torrent_path).unwrap();
-        println!("{}", decoded_value);
+        println!("{}", decoded_value.0);
+        println!("Info Hash: {}", decoded_value.1);
     } else {
         eprintln!("unknown command: {}", args[1])
     }
